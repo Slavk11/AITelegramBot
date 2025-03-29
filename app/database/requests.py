56eb853deab84aa454141038f53b6295aefa90b8
Stanislav Sazonov <slavk11@yandex.ru>
2025-03-29 12:25:24 +0300

    Add user notification system

--- app/database/requests.py
+++ app/database/requests.py
@@ -1,12 +1,26 @@
 from app.database.models import async_session
 from app.database.models import User
 from sqlalchemy import select, update, delete, desc
+def connection(func):
+    async def inner(*args, **kwargs):
+        async with async_session() as session:
+            return await func(session, *args, **kwargs)
+    return inner
 
-
-async def set_user(tg_id):
+@connection
+async def set_user(session, tg_id):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
 
         if not user:
             session.add(User(tg_id=tg_id))
             await session.commit()
+
+
+@connection
+async def get_user(session, tg_id):
+    return await session.scalar(select(User).where(User.tg_id == tg_id))
+
+@connection
+async def get_users(session):
+    return await session.scalars(select(User))
